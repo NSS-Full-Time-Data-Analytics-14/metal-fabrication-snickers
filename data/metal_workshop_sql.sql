d8f4deab1d97e8f31c7798afe11b1bf809fcbf6b
unknown <j2das1>
2024-11-19 13:59:25 -0800

    Second day metal project second push

--- data/metal_workshop_sql.sql
+++ data/metal_workshop_sql.sql
@@ -41,3 +41,48 @@ GROUP BY jmo_job_operation_id, jmo_estimated_production_hours, jmo_completed_pro
 SELECT j.jmp_customer_organization_id,j23.jmo_job_operation_id
 FROM jobs as j
 INNER JOIN job_operations_2023 as j23 ON j.jmp_job_id = j23.jmo_job_id
+
+
+SELECT *
+FROM jobs
+INNER JOIN job_operations_2023 as j23 ON jobs.jmp_job_id = j23.jmo_job_id
+INNER JOIN sales_order_job_links as sojl ON jobs.jmp_job_id = sojl.omj_job_id
+INNER JOIN sales_orders as so ON so.omp_sales_order_id = sojl.omj_sales_order_id
+
+-- Production hours vs Actual hours spent 2023
+SELECT jmo_part_id, SUM(jmo_estimated_production_hours)::decimal as estimated_hours_2023, SUM(jmo_actual_production_hours)::decimal as actual_hours_2023, SUM(jmo_estimated_production_hours-jmo_actual_production_hours)::decimal as production_hours_difference_2023
+FROM job_operations_2023
+WHERE jmo_part_id IS NOT null AND jmo_actual_production_hours > jmo_estimated_production_hours
+GROUP BY jmo_part_id
+ORDER BY production_hours_difference_2023
+
+-- Production hours vs Actual hours spend 2024
+SELECT jmo_part_id, SUM(jmo_estimated_production_hours)::decimal as estimated_hours_2024, SUM(jmo_actual_production_hours)::decimal as actual_hours_2024, SUM(jmo_estimated_production_hours-jmo_actual_production_hours)::decimal as production_hours_difference_2024
+FROM job_operations_2024
+WHERE jmo_part_id IS NOT null AND jmo_actual_production_hours > jmo_estimated_production_hours
+GROUP BY jmo_part_id
+ORDER BY production_hours_difference_2024
+
+
+
+SELECT jmo_part_id, COUNT(jmo_job_id) as total_parts, 
+FROM job_operations_2023
+GROUP BY jmo_part_id, jmo_actual_production_hours
+ORDER By total_parts DESC
+
+SELECT jmo_part_id, COUNT(jmo_part_id) as parts_count, jmo_completed_production_hours, jmo_actual_production_hours, omp_order_subtotal_base
+FROM jobs
+INNER JOIN job_operations_2023 as j23 ON jobs.jmp_job_id = j23.jmo_job_id
+INNER JOIN sales_order_job_links as sojl ON jobs.jmp_job_id = sojl.omj_job_id
+INNER JOIN sales_orders as so ON so.omp_sales_order_id = sojl.omj_sales_order_id
+GROUP BY jmo_part_id, jmo_completed_production_hours,jmo_actual_production_hours,omp_order_subtotal_base
+ORDER BY parts_count DESC
+
+--
+SELECT jmo_part_id, COUNT(jmo_part_id) as part_count, SUM(jmo_actual_production_hours)::decimal as total_production_hours, SUM(omp_order_subtotal_base)::decimal AS subtotal_base
+FROM jobs
+INNER JOIN job_operations_2023 as j23 ON jobs.jmp_job_id = j23.jmo_job_id
+INNER JOIN sales_order_job_links as sojl ON jobs.jmp_job_id = sojl.omj_job_id
+INNER JOIN sales_orders as so ON so.omp_sales_order_id = sojl.omj_sales_order_id
+WHERE jmo_part_id IS NOT NULL
+GROUP BY jmo_part_id, jmo_actual_production_hours, omp_order_subtotal_base
